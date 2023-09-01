e2f6aa2302a19bc4e21316f5436c23c0587d0359
Victor Queiroz <victorcqueirozg@gmail.com>
2023-09-01 17:29:16 -0300

    add E2E test for updating todo item

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,3 +1,5 @@
+import { randomTodoItemTitle } from 'cypress/utilities';
+
 describe('My First Test', () => {
   it('should open initial project page', () => {
     cy.visit('/');
@@ -5,21 +7,36 @@ describe('My First Test', () => {
   });
   it('should create todo item', () => {
     cy.visit('/');
-    const randomTodoItemTitle = `Test Todo: ${Math.ceil(
-      Math.random() * 100000000
-    )}`;
-    cy.get('app-todo-create-form input[placeholder="Title"]').type(
-      randomTodoItemTitle
-    );
+    const title = randomTodoItemTitle();
+    cy.get('app-todo-create-form input[placeholder="Title"]').type(title);
     cy.intercept({
       method: 'POST',
       url: /\/api\/CreateTodoItem/,
     }).as('createTodoItem');
     cy.get('[data-test-id="create-new-todo-item-button"]').click();
     cy.wait('@createTodoItem');
-    cy.get('#existing-todo-item-title').should(
-      'have.value',
-      randomTodoItemTitle
-    );
+    cy.get('#existing-todo-item-title').should('have.value', title);
+  });
+  it('should update todo item', () => {
+    cy.visit('/');
+    const title = randomTodoItemTitle();
+    cy.get('#new-todo-item-title').type(title);
+    cy.intercept({
+      method: 'POST',
+      url: /\/api\/CreateTodoItem/,
+    }).as('createTodoItem');
+    cy.get('[data-test-id="create-new-todo-item-button"]').click();
+    cy.wait('@createTodoItem');
+    const titleInput = cy.get('#existing-todo-item-title');
+    titleInput.should('have.value', title);
+    const title2 = randomTodoItemTitle();
+    cy.intercept({
+      method: 'PUT',
+      url: /\/api\/UpdateTodoItem/,
+    }).as('updateTodoItem');
+    titleInput.clear().type(title2).blur();
+    cy.wait('@updateTodoItem');
+    titleInput.should('have.value', title2);
+    titleInput.should('not.have.value', title);
   });
 });
---
+++ cypress/utilities.ts
@@ -0,0 +1,3 @@
+export function randomTodoItemTitle() {
+  return `Test Todo: ${Math.ceil(Math.random() * 100000000)}`;
+}
